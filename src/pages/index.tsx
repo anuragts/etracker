90ef12ff2d55985e6e9a9443f9eb8e6eca48423e
Anurag30112003 <anuragsharma011011@gmail.com>
2022-10-18 20:00:27 +0530

    setup ++

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <div className='text-3xl font-bold underline'>
+      Hello World
+    </div>
+    </>
+  )
+}
+
+export default Home
