c8a74ae45119625116cb5f93c1194f0c4fd005b9
Anurag30112003 <anuragsharma011011@gmail.com>
2022-10-20 02:19:55 +0530

    login hald

---
+++ src/pages/api/users/login.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest,NextApiResponse } from "next";
+import { prisma } from "../../../db/client";
+
+export default async function login(req:NextApiRequest,res:NextApiResponse) {
+    const { email, password } = req.body;
+    const exists = prisma.user.findMany({ where: { email } });
+    if (!email || !password) {
+        res.status(400).send("Please fill all fields");
+    } else if ((await exists).length === 0) {
+        res.status(400).send("User does not exist");
+    } else {
+        const user = await prisma.user.findMany({ where: { email } });
+        res.status(200).json(user);
+        
+        // more to do here
+    }
+}
